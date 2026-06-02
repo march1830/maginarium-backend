895af8cd9a684fddaf95a9acaa21a9a89ed564c9
Marat Gaisin <maratgaysin76@gmail.com>
2026-06-02 14:45:21 +0400

    get lobby endpoint join game endpoint

--- src/main/java/com/maginarium/MaginariumBackendApplication.java
+++ src/main/java/com/maginarium/MaginariumBackendApplication.java
@@ -1,5 +1,7 @@
 package com.maginarium;
 
+import com.maginarium.controller.GameController;
+import com.maginarium.storage.PlayerStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/maginarium/controller/GameController.java
+++ src/main/java/com/maginarium/controller/GameController.java
@@ -1,9 +1,11 @@
 package com.maginarium.controller;
 
 import com.maginarium.model.Player;
+import com.maginarium.model.Room;
 import com.maginarium.storage.PlayerStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,36 @@ public class GameController {
     private final PlayerStorage playerStorage;
 
     @PostMapping("/game/create")
-    public String createGame(@RequestParam String hostName) {
-        String roomCode = "NEW1";
-        Player host = new Player(hostName, true);
-        playerStorage.createRoom(roomCode, host);
-        return roomCode;
+    public ResponseEntity<String> createGame(@RequestParam String name) {
+        Player host = new Player(name, true);
+        String roomCode = playerStorage.createRoom(host);
+        return ResponseEntity.ok(roomCode);
     }
 
+    @GetMapping("/game/lobby")
+    public ResponseEntity<Room> getLobby(@RequestParam String roomCode) {
+        if (!playerStorage.existRoom(roomCode)) {
+            return ResponseEntity.notFound().build();
+        }
+        Room foundRoom = playerStorage.getRoom(roomCode);
+        return ResponseEntity.ok(foundRoom);
+    }
+
+    @PostMapping("/game/join")
+    public ResponseEntity<String> joinGame(@RequestParam String name, @RequestParam String roomCode) {
+        if (!playerStorage.existRoom(roomCode)) {
+            return ResponseEntity.notFound().build();
+        }
+        Room foundRoom = playerStorage.getRoom(roomCode);
+        synchronized (foundRoom) {
+            for (Player player : foundRoom.getPlayers()) {
+                if (player.name().equalsIgnoreCase(name)) {
+                    return ResponseEntity.badRequest().body("Name is already taken");
+                }
+            }
+            Player player = new Player(name, false);
+            foundRoom.addPlayer(player);
+            return ResponseEntity.ok(roomCode);
+        }
+    }
 }
